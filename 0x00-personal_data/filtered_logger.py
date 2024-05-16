1f95015ac63bae4041f970ef5dcfd9553aaddfdb
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-16 20:31:36 +0100

    0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''Handle Personal Data.'''
+import re
+from typing import List
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str) -> str:
+    """Filter sensitive data in a message.
+    """
+    import re
+
+def filter_datum(fields, redaction, message, separator):
+    for field in fields:
+        message = re.sub(fr'\b{field}\b', redaction, message)
+    return message
