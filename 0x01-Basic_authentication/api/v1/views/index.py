18be084aa82ca7ed27773b3a9ebfb1c3ce76d12f
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-20 19:45:44 +0100

    task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -20,6 +20,12 @@ def unauthorized() -> None:
     abort(401)
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """Triggers a 403 Forbidden error for testing purposes"""
+    abort(403)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
