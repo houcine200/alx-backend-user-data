f3c2a0768974c569497666d1e7d1c46b755409e8
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-21 16:13:11 +0100

    task 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module for Basic Authentication """
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """ BasicAuth class that inherits from Auth
+    """
+    pass
