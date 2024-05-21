77c883d438928ea401ae8fa8394f8aa5684f5dcd
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-21 19:33:20 +0100

    task 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -50,3 +50,26 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extracts the user email and password from the Base64 decoded value.
+
+        Args:
+            decoded_base64_authorization_header (str):
+            The Base64 decoded value.
+
+        Returns:
+            tuple: The user email and password.
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email = decoded_base64_authorization_header.split(":")[0]
+        password = decoded_base64_authorization_header.split(":")[1]
+        return email, password
