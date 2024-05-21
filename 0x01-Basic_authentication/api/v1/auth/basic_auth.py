95020d202cdc2b7f768726e1c5c1d35c137a4ca3
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-21 20:40:51 +0100

    10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -98,7 +98,7 @@ class BasicAuth(Auth):
         if not users or users == []:
             return None
 
-        if not users[0].is_valid_password(user_pwd):
-            return None
-
-        return users[0]
+        for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        return None
