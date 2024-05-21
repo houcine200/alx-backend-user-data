72d6daa27e96c0284aede5f3cdccc5b28ba58279
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-21 20:34:40 +0100

    10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -95,7 +95,7 @@ class BasicAuth(Auth):
             return None
 
         users = User.search({'email': user_email})
-        if not users:
+        if not users or users == []:
             return None
 
         user = users[0]
