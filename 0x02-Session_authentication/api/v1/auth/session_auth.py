62d70a53878123302468d1f2782e8ba17e13b4b6
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-22 20:41:25 +0100

    task 2 pycodestyle

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,10 @@
 from api.v1.auth.auth import Auth
 import uuid
 
+
 class SessionAuth(Auth):
     """ SessionAuth class for session authentication """
-    
+
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
