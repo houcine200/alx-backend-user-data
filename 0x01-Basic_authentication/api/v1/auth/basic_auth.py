4806eb8c1c7b58f483f70d3207e794b7a36e1514
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-22 12:36:25 +0100

    10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -89,19 +89,14 @@ class BasicAuth(Auth):
         Returns:
             User: The User instance if found, else None.
         """
-        if user_email is None or not isinstance(user_email, str):
-            return None
-        if user_pwd is None or not isinstance(user_pwd, str):
-            return None
-
-        users = User.search({'email': user_email})
-        if not users or users == []:
+        if user_email is None or not isinstance(user_email, str) or \
+                user_pwd is None or not isinstance(user_pwd, str):
             return None
 
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
-        return None
+        user = User().search({"email": user_email})
+        if user == [] or not user[0].is_valid_password(user_pwd):
+            return None
+        return user[0]
 
     def current_user(self, request=None) -> User:
         """Retrieve the User instance for a request."""
