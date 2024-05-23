9cb7daaeff16e65e09f2b4e8d256612416984123
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-23 15:37:04 +0100

    task 6 get user by cookie value

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ SessionAuth module for handling session authentication """
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -24,3 +25,11 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Retrieve a User instance based on a cookie value """
+        session_id = self.session_cookie(request)
+
+        user_id = self.user_id_for_session_id(session_id)
+        user = User.get(user_id)
+        return user
