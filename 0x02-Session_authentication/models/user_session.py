21b384c502d15f62be6296c80badcf4fcbd1a9b5
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-25 16:25:51 +0100

    10 db

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" UserSession module
+"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """UserSession class to manage user sessions stored in the database."""
+    def __init__(self, *args: list, **kwargs: dict):
+        """Initialize a UserSession instance with user_id and session_id."""
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
