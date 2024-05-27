0ed11963ccfd2d4144acb23099d1240b1dd9fdf4
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-27 20:56:04 +0100

    task 5 register

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+
+from user import User
+
 
 def _hash_password(password: str) -> bytes:
     """
@@ -11,3 +16,27 @@ def _hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password_bytes, salt)
 
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+
+        hashed_password = _hash_password(password)
+
+        user = self._db.add_user(email=email, hashed_password=hashed_password)
+
+        return user
