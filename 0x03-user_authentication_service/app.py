3a82ebde416344eb37b7ac22c40eed7b2be71362
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-29 15:49:11 +0100

    log task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -44,12 +44,10 @@ def login():
     if not AUTH.valid_login(email, password):
         abort(401)
 
-    session_id = AUTH.create_session(email)
-    if not session_id:
-        abort(401)
-    
-    response = make_response(jsonify({"email": email, "message": "logged in"}))
-    response.set_cookie("session_id", session_id)
+    else:
+        session_id = AUTH.create_session(email)
+        response = make_response(jsonify({"email": email, "message": "logged in"}))
+        response.set_cookie("session_id", session_id)
 
     return response
 
