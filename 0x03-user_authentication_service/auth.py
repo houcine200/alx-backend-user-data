5626a8e453a9da8170dfe8fd5c6922e9a6526546
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-30 19:23:29 +0100

    18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -116,14 +116,12 @@ class Auth:
         Update a user's password using the reset token.
         """
         if reset_token is None or password is None:
-            return None
+            raise ValueError
 
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
             raise ValueError
 
-        hashed_password = _hash_password(password)
-        self._db.update_user(user.id,
-                             hashed_password=hashed_password,
-                             reset_token=None)
+        pw = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=pw, reset_token=None)
