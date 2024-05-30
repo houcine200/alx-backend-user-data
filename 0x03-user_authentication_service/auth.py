46d9d62a7b7e9b41cc6c59af716b5a0d53ea2fb9
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-30 19:13:38 +0100

    task 18 update user pwd

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -110,3 +110,18 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Update a user's password using the reset token.
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            # nvalid reset token
+            raise ValueError
+
+        hashed_password = _hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=hashed_password,
+                             reset_token=None)
