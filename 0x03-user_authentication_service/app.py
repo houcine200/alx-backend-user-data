b823f32984e238fd0a33f8a4a6497a0d04fdf229
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-30 15:29:45 +0100

    17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,5 +82,12 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """Generate a reset password token for a user."""
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
