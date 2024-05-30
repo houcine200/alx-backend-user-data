211c65a447d2dc8b61bc474d693ea52ee27c17ae
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-30 19:05:58 +0100

    all checkers wrong :(

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,36 +4,25 @@ from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 import uuid
 from typing import Optional
-
 from user import User
-
-
 def _hash_password(password: str) -> bytes:
     """
     Hashes a password string using bcrypt.
     """
     password_bytes = password.encode('utf-8')
-
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password_bytes, salt)
-
     return hashed_password
-
-
 def _generate_uuid() -> str:
     """
     Generate a new UUID.
     """
     return str(uuid.uuid4())
-
-
 class Auth:
     """Auth class to interact with the authentication database.
     """
-
     def __init__(self):
         self._db = DB()
-
     def register_user(self, email: str, password: str) -> User:
         """
         Register a new user.
@@ -43,13 +32,9 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             pass
-
         hashed_password = _hash_password(password)
-
         user = self._db.add_user(email=email, hashed_password=hashed_password)
-
         return user
-
     def valid_login(self, email: str, password: str) -> bool:
         """
         Validate login credentials.
@@ -62,7 +47,6 @@ class Auth:
                 return False
         except NoResultFound:
             return False
-
     def create_session(self, email: str) -> str:
         """
         Create a session for the user with the given email.
@@ -76,7 +60,6 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
-
     def get_user_from_session_id(
             self, session_id: Optional[str]) -> Optional[User]:
         """
@@ -85,14 +68,11 @@ class Auth:
         """
         if session_id is None:
             return None
-
         try:
             user = self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
-
         return user
-
     def destroy_session(self, user_id: int) -> None:
         """
         Destroy a user's session.
@@ -110,5 +90,3 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
-
-    
